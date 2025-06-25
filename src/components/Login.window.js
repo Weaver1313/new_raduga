6f944ac92a63f67770c8ea179c0dacf4e3b05ddd
ismailshukurov <ismail.shukurov.2002@gmail.com>
2025-06-25 20:52:01 +0300

    add test reset password

--- src/components/Login.window.js
+++ src/components/Login.window.js
@@ -17,6 +17,24 @@ export class LoginWindow {
         return this.page.getByRole("button", { name: "Login" });
     }
 
+    get buttonResetPassword() {
+        return this.page.getByRole("button", { name: "Forgot password?" });
+    }
+
+    get inputEmail() {
+        return this.page.getByRole("textbox");
+    }
+
+    get buttonSendResetPassword() {
+        return this.page.getByRole("button", { name: "Reset" });
+    }
+
+    get messageInfoResetPassword() {
+        return this.page.getByText(
+            "The link to change your password was sent to the email"
+        );
+    }
+
     async inputLoginFill() {
         await this.inputLogin.fill(process.env.LOGIN);
         await expect(this.inputLogin).toHaveValue(process.env.LOGIN);
@@ -29,5 +47,21 @@ export class LoginWindow {
 
     async buttonSignInClick() {
         await this.buttonSignIn.click();
+    }
+
+    async buttonResetPasswordClick() {
+        await this.buttonResetPassword.click();
+    }
+
+    async inputEmailFill() {
+        await this.inputEmail.fill(process.env.LOGIN);
+        await expect(this.inputEmail).toHaveValue(process.env.LOGIN);
+    }
+
+    async buttonSendResetPasswordClick() {
+        await this.buttonSendResetPassword.click();
+        await expect(this.messageInfoResetPassword).toHaveText(
+            "The link to change your password was sent to the email"
+        );
     }
 }
